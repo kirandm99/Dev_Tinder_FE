daf016c54584b469e43412efcb53c655596334b6
kiran <kirandm99@gmail.com>
2026-07-16 23:42:41 +0530

    razor pay setup

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,4 +1,39 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
+
 const Premium = () => {
+  const handleBuyClick = (type) => {
+    const order = axios.post(
+      BASE_URL + "/payment/create",
+      { membershipType: type },
+      { withCredentials: true },
+    );
+
+    const { amount, keyId, currency, notes, orderId } = order.data;
+
+    //open razorpay dialoge box
+
+    const options = {
+      key: keyId,
+      amount,
+      currency,
+      name: "Dev Tinder",
+      description: "Connect one developer to other developers",
+      order_id: orderId,
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+        contact: "9999999999",
+      },
+      theme: {
+        color: "#F37254",
+      },
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+
   return (
     <div className="m-10">
       <div className="flex w-full">
@@ -10,7 +45,12 @@ const Premium = () => {
             <li>- Blue Tick</li>
             <li>- 3 months</li>
           </ul>
-          <button className="btn btn-secondary">Buy Now</button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleBuyClick("Silver")}
+          >
+            Buy Now
+          </button>
         </div>
         <div className="divider divider-horizontal">OR</div>
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -21,7 +61,12 @@ const Premium = () => {
             <li>- Blue Tick</li>
             <li>- 6 months</li>
           </ul>
-          <button className="btn btn-secondary">Buy Now</button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleBuyClick("Gold")}
+          >
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
