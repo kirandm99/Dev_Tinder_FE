0a5211c5b23bcea4a6c43bfd5862ecf308a0903f
kiran <kirandm99@gmail.com>
2026-07-17 23:31:57 +0530

    implemetde handler logic to verify payment

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,9 +1,21 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constant";
+import { useState } from "react";
 
 const Premium = () => {
-  const handleBuyClick = (type) => {
-    const order = axios.post(
+  const [isUserPremium, setIsUserPremium] = useState(false);
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/payment/verify", {
+      withCredentials: true,
+    });
+
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
+  const handleBuyClick = async (type) => {
+    const order = await axios.post(
       BASE_URL + "/payment/create",
       { membershipType: type },
       { withCredentials: true },
@@ -28,13 +40,16 @@ const Premium = () => {
       theme: {
         color: "#F37254",
       },
+      handler: verifyPremiumUser,
     };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
 
-  return (
+  return isUserPremium ? (
+    "You Are Already Premium User"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
